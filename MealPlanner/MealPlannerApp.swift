8c5608689a18ca3b2cdbad7b1bad3369ea6d940d
Bhagyashree <bagsphadnis@gmail.com>
2026-03-14 15:55:33 +0530

    Initial Commit

---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by Bhagyashree Phadnis on 14/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
