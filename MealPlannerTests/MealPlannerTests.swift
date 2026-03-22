f9d982b079eda5dad02b020ae58c13ffde7265f0
Bhagyashree <bagsphadnis@gmail.com>
2026-03-22 13:07:57 +0530

    ui changes

---
+++ MealPlannerTests/MealPlannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MealPlannerTests.swift
+//  MealPlannerTests
+//
+//  Created by Bhagyashree Phadnis on 21/03/26.
+//
+
+import Testing
+
+struct MealPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
